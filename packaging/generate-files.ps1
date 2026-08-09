9c65366f2288e1be2c12e6667d54fc95c15d983d
Marci <Marci@DESKTOP-IR7RCO3>
2026-08-10 00:20:17 +0800

    fix: WiX 文件清单支持子目录树（web/dist/assets 不再平铺），修复安装后 JS/CSS 404 白屏

--- packaging/generate-files.ps1
+++ packaging/generate-files.ps1
@@ -1,4 +1,4 @@
-﻿# 生成 WiX 文件清单（fragment wxs）：根目录文件 + web/dist 子目录
+﻿# 生成 WiX 文件清单（fragment wxs）：根目录文件 + web/dist（含 assets 子目录树）
 param(
     [string]$PublishDir = '',
     [string]$OutFile = ''
@@ -28,32 +28,66 @@ function Add-FileLine([System.Text.StringBuilder]$sb, [int]$idx, [string]$guid, 
     [void]$sb.AppendLine('      </Component>')
 }
 
+$allFiles = Get-ChildItem -Path $PublishDir -Recurse -File
+# 先收集 web 文件，确定子目录集合
+$webFiles = @($allFiles | Where-Object {
+    $r = $_.FullName.Substring($PublishDir.Length).TrimStart('\') -replace '\\', '/'
+    $r -like 'web/dist/*'
+})
+$webSubDirs = @{}   # relSubDir -> dirId
+foreach ($f in $webFiles) {
+    $rel = $f.FullName.Substring($PublishDir.Length).TrimStart('\') -replace '\\', '/'
+    $relWeb = $rel.Substring('web/dist/'.Length)
+    $slash = $relWeb.IndexOf('/')
+    if ($slash -ge 0) {
+        $sub = $relWeb.Substring(0, $slash)
+        if (-not $webSubDirs.ContainsKey($sub)) {
+            $dirId = 'WebDir_' + (($sub -replace '[^a-zA-Z0-9]', '') + '_' + (Get-Random -Maximum 99999))
+            $webSubDirs[$sub] = $dirId
+        }
+    }
+}
+
 $sb = New-Object System.Text.StringBuilder
 [void]$sb.AppendLine('<?xml version="1.0" encoding="utf-8"?>')
 [void]$sb.AppendLine('<Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">')
 [void]$sb.AppendLine('  <Fragment>')
 [void]$sb.AppendLine('    <DirectoryRef Id="INSTALLFOLDER">')
 [void]$sb.AppendLine('      <Directory Id="WebDir" Name="web">')
-[void]$sb.AppendLine('        <Directory Id="WebDistDir" Name="dist" />')
+[void]$sb.AppendLine('        <Directory Id="WebDistDir" Name="dist">')
+foreach ($sub in $webSubDirs.Keys) {
+    [void]$sb.AppendLine('          <Directory Id="' + $webSubDirs[$sub] + '" Name="' + $sub + '" />')
+}
+[void]$sb.AppendLine('        </Directory>')
 [void]$sb.AppendLine('      </Directory>')
 [void]$sb.AppendLine('    </DirectoryRef>')
-[void]$sb.AppendLine('    <ComponentGroup Id="AppFiles" Directory="INSTALLFOLDER">')
 
 $webSb = New-Object System.Text.StringBuilder
-[void]$webSb.AppendLine('    <ComponentGroup Id="WebFiles" Directory="WebDistDir">')
+[void]$webSb.AppendLine('    <ComponentGroup Id="WebFiles">')
 
 $index = 0
 $webIndex = 0
-Get-ChildItem -Path $PublishDir -Recurse -File | ForEach-Object {
-    $rel = $_.FullName.Substring($PublishDir.Length).TrimStart('\')
-    $rel = $rel -replace '\\', '/'
+foreach ($f in $allFiles) {
+    $rel = $f.FullName.Substring($PublishDir.Length).TrimStart('\') -replace '\\', '/'
     $guid = New-GuidFromPath $rel
-    $isWeb = $rel.StartsWith('web/dist/')
     if ($rel -eq 'LabelFrame.WinHost.exe') {
         Add-FileLine $sb $index $guid $rel ' Id="WinHostExe"' 'f'
         $index++
-    } elseif ($isWeb) {
-        Add-FileLine $webSb $webIndex $guid $rel (' Id="wf' + $webIndex + '"') 'w'
+    } elseif ($rel -like 'web/dist/*') {
+        $relWeb = $rel.Substring('web/dist/'.Length)
+        $slash = $relWeb.IndexOf('/')
+        if ($slash -ge 0) {
+            $sub = $relWeb.Substring(0, $slash)
+            $dirId = $webSubDirs[$sub]
+            [void]$sb.AppendLine('    <DirectoryRef Id="' + $dirId + '">')
+            Add-FileLine $sb $webIndex $guid $rel (' Id="wf' + $webIndex + '"') 'w'
+            [void]$sb.AppendLine('    </DirectoryRef>')
+        } else {
+            [void]$sb.AppendLine('    <DirectoryRef Id="WebDistDir">')
+            Add-FileLine $sb $webIndex $guid $rel (' Id="wf' + $webIndex + '"') 'w'
+            [void]$sb.AppendLine('    </DirectoryRef>')
+        }
+        [void]$webSb.AppendLine('      <ComponentRef Id="w' + $webIndex + '" />')
         $webIndex++
     } else {
         Add-FileLine $sb $index $guid $rel (' Id="ff' + $index + '"') 'f'
@@ -61,6 +95,8 @@ Get-ChildItem -Path $PublishDir -Recurse -File | ForEach-Object {
     }
 }
 
+[void]$sb.AppendLine('    <ComponentGroup Id="AppFiles" Directory="INSTALLFOLDER">')
+for ($i = 0; $i -lt $index; $i++) { [void]$sb.AppendLine('      <ComponentRef Id="f' + $i + '" />') }
 [void]$sb.AppendLine('    </ComponentGroup>')
 [void]$webSb.AppendLine('    </ComponentGroup>')
 [void]$sb.Append($webSb.ToString())
